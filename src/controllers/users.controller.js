d4ae3bfeba293fc703237fbf01c108b8b910e7dd
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-08-05 17:43:02 -0300

    SignIn + SignUp

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,17 +1,55 @@
+import bcrypt from "bcrypt";
+import { v4 as tokenGeneretor } from "uuid";
 import { db } from "../database/database.connection.js";
 /* Games */
 
 export async function signUp(req, res) {
+  const { name, email, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) return res.status(422).send(" As senhas apresentadas são diferentes. ")
+
+  const hash = bcrypt.hashSync(password, 10);
   try {
-    res.send("Resposta signUp");
+    let usuarioPreExistente = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+    if (usuarioPreExistente.rows.length > 0) return res.status(422).send(" Esse email já está cadastrado ");
+
+    let insert = await db.query(`
+    INSERT INTO users 
+      (name, email, password)
+    VALUES 
+      ($1, $2, $3);
+    `,[name, email, hash])
+
+    res.status(201).send("SignUp efetuado com sucesso");
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function signIn(req, res) {
+  const { email, password} = req.body;
+  const token = tokenGeneretor();
+  
   try {
-    res.send("Resposta signIn");
+    let usuario = (await db.query(`SELECT * FROM users WHERE email = $1;`,[email])).rows[0];
+    if(!usuario) {return res.status(401).send(" O email enviado não está cadastrado. ")};
+
+    const senhaEstaCorreta = bcrypt.compareSync(password, usuario.password);
+    if(!senhaEstaCorreta) {return res.status(401).send(" A senha enviada não está correta. ")};
+
+    let  usuarioPossuiToken = await db.query(`SELECT * FROM sessions WHERE "userId" = $1;`,[usuario.id]);
+    /**/ usuarioPossuiToken = usuarioPossuiToken.rows;
+
+    if (usuarioPossuiToken.length !== 0) { 
+      return res.send({token: usuarioPossuiToken[0].token}) 
+    }
+    if (usuarioPossuiToken.length === 0) { 
+      await db.query(`INSERT INTO sessions("userId", token) VALUES($1, $2);`,
+      [usuario.id, token]);
+      return res.send({token})
+      return(res.send("usuario n possui token"));
+    }
+    res.send(usuarioPossuiToken);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
-import { Router } from "express"
-import { signUp, signIn, usersMe, ranking } from "../controllers/users.controller.js"
+import { Router } from "express";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { signUpSchema, signInSchema } from "../schemas/users.schemas.js";
+import { signUp, signIn, usersMe, ranking } from "../controllers/users.controller.js";
 
 const usersRouter = Router();
 
-usersRouter.post("/signup", signUp);
-usersRouter.post("/signin", signIn);
+usersRouter.post("/signup", validateSchema(signUpSchema), signUp);
+usersRouter.post("/signin", validateSchema(signInSchema), signIn);
 usersRouter.get("/users/me", usersMe);
 usersRouter.get("/ranking", ranking);
 
